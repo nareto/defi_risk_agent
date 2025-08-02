778f2d7b509b449ba9b9771cdaf5f7e2d31d40df
renato <bnh@terra>
2025-08-02 22:54:27 +0200

    cleanup

--- src/data_layer_old/portfolio.py
+++
@@ -1,9 +0,0 @@
-from src.providers.alchemy import api_alchemy_portfolio as get_alchemy_portfolio
-
-def get_portfolio(wallet):
-    alchemy_portfolio = get_alchemy_portfolio(wallet)
-    totUsd = sum(t['usd_value'] for t in alchemy_portfolio)
-    out = alchemy_portfolio
-    for token in out:
-        token['portfolio_percentage_usd'] = float(token['usd_value']/totUsd)
-    return out
